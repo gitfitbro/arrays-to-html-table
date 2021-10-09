cee2e7ad0361f6e23fc09d037dd65b076b50f265
Sirrele Steinfeld <mssteinfeld@gmail.com>
2021-10-09 05:09:46 -0700

    Fixed Bugs
    refactor implementation to correctly compares the source and target object and determine changed, deleted, added properties
    Created a table with color meaning
    removed unused functions
    Dynamically setting color

--- diff.js
+++ diff.js
@@ -1,11 +1,10 @@
-
 /**
  * @function arrayDiffToHtmlTable
  * @param {Array} prevArray
  * @param {Array} currArray
  * @returns {String} htmlTable
  * TODO: Move most of these comments to a README.md file
- * 
+ *
  * The function will take two arguments
  * [x] (prevArray, currArray)
  * [x] flattens the objects inside of prevArray and currArray to 1 level of depth,
@@ -27,7 +26,7 @@
  * --> If you create global scope for 1 individual diff between prevArray to currArray you could cause other tests to fail
  **/
 
-module.exports.arrayDiffToHtmlTable = function( prevArray, currArray) {
+module.exports.arrayDiffToHtmlTable = function (prevArray, currArray) {
   // flattens the objects inside of prevArray and currArray
   const flattenPreArray = prevArray.map(obj => flattenObject(obj))
   const flattenCurrArray = currArray.map(obj => flattenObject(obj))
@@ -60,18 +59,23 @@ const timed = f => {
 
 const flattenObject = (obj, keySeparator = '.', prefix = '') => {
   const flattenRecursive = (obj, parentProperty = '', propertyMap = {}) => {
-    for(const [key, value] of Object.entries(obj)){
-      const pre = prefix.length ? prefix + '.' : '';
-      const property = parentProperty ? `${parentProperty}${keySeparator}${key}` : key;
-      if(value && typeof value === 'object'){
-        flattenRecursive(value, parentProperty.length ? parentProperty + '.' + key : key, propertyMap);
+    for (const [key, value] of Object.entries(obj)) {
+      const property = parentProperty.length ? parentProperty + keySeparator + key : key
+      if (value && typeof value === 'object') {
+        flattenRecursive(
+          value,
+          property,
+          propertyMap
+        )
       } else {
-        propertyMap[parentProperty.length ? parentProperty + '.' + key : key] = value;
+        propertyMap[
+          property
+        ] = value
       }
     }
-    return propertyMap;
-  };
-  return flattenRecursive(obj);
+    return propertyMap
+  }
+  return flattenRecursive(obj)
 }
 
 const merge = (target, ...sources) => {
@@ -85,15 +89,6 @@ const merge = (target, ...sources) => {
   return target
 }
 
-const isValidObj = obj => {
-  // TODO: Potentially add more validation checks
-  let isValid = false
-  if (obj && Object.keys(obj).length > 0) {
-    isValid = true
-  }
-  return isValid
-}
-
 const getTableColumnValues = array => {
   let columns = []
   mappedArray = array.map(obj => {
@@ -104,53 +99,43 @@ const getTableColumnValues = array => {
   return columns
 }
 
-const getDiff = (prevArray, currArray) => {
-  const flattenPreArray = flattenObject(prevArray)
-  const flattenCurrArray = flattenObject(currArray)
-  // Get values that have changed between the two arrays
-  const diff = {}
-  Object.keys(flattenCurrArray).forEach(key => {
-    if (flattenPreArray[key] != flattenCurrArray[key]) {
-      diff[key] = flattenCurrArray[key]
-    }
-  })
-  return Object.keys(diff)
-}
-
-const getRemovedKeys = (prevArray, currArray) => {
-  const flattenPreArray = flattenObject(prevArray)
-  const flattenCurrArray = flattenObject(currArray)
-  // Get values that have been deleted from flattenPreArray to flattenCurrArray
-  const removedKeys = []
-  Object.keys(flattenPreArray).forEach(key => {
-    if (!flattenCurrArray[key]) {
-      removedKeys.push(key)
-    }
-  })
-  return removedKeys
-}
-
-const isChangedValue = (prevArray, currArray, keyValue) => {
+const isChangedValue = (prevArray, currArray, keyValue, id) => {
   let isChanged = {
-    bold: false,
-    deleted: false
+    changed: false,
+    deleted: false,
+    added: false
   }
   if (keyValue) {
-    const changedProperties = getDiff(prevArray, currArray)
-    let isValueChanged = changedProperties.some(changedProperty => {
-      return changedProperty.includes(keyValue)
-    })
-    isChanged.bold = isValueChanged
-    let isValueDeleted = getRemovedKeys(prevArray, currArray).some(key =>
-      keyValue.includes(key)
-    )
-    isChanged.deleted = isValueDeleted
+    // find the object in the currArray with the matching id
+    const currObj = currArray.find(obj => obj['_id'] === id)
+    // find the object in the prevArray with the matching id
+    const prevObj = prevArray.find(obj => obj['_id'] === id)
+    // if the object is found in the currArray, check if the value has changed
+    if (currObj && prevObj) {
+      const currValue = currObj[keyValue]
+      const prevValue = prevObj[keyValue]
+      // if the value has been deleted, set isChanged.deleted to true
+      !currValue ? (isChanged.deleted = true) : null
+      // if the value has changed, set isChanged.changed to true
+      currValue !== prevValue ? (isChanged.changed = true) : null
+      // if the value has been added, set isChanged.added to true
+      currValue && !prevValue ? (isChanged.added = true) : null
+    } else if (currObj && !prevObj) {
+      // if the object is found in the currArray, but not in the prevArray, set isChanged.added to true
+      isChanged.added = true
+    } else if (!currObj && prevObj) {
+      // if the object is not found in the currArray, but is found in the prevArray, set isChanged.deleted to true
+      isChanged.deleted = true
+    }
   }
   return isChanged
 }
 
 const generateHtmlTable = (columns, flattenCurrArray, flattenPreArray) => {
-  let htmlTable = `<table style="width:100%"><tr>`
+  let htmlTable = `
+  ${generateColorTable()}
+  <br>
+  <table style="width:100%"><tr>`
   // Loop through the columns and update the table
   columns.forEach(column => {
     htmlTable += `<th data-column=${column}>${column}</th>`
@@ -160,23 +145,74 @@ const generateHtmlTable = (columns, flattenCurrArray, flattenPreArray) => {
   flattenCurrArray.forEach(rowValue => {
     htmlTable += `<tr>`
     columns.forEach(key => {
-      let changedProperties = getDiff(flattenPreArray, flattenCurrArray)
-      let changedVal = changedProperties.some(changedProperty => {
-        changedProperty.indexOf(key) !== -1
-      })
-      let keyChangeMeta = isChangedValue(flattenPreArray, flattenCurrArray, key)
-      // Any values that have changed from the flattenPreArray to the flattenCurrArray (ie field value changed or is a new key altogether) should be bolded.
-      // In the case that the value has been removed altogether from the prevArray to the currArray, you will write out the key in bold DELETED.
-      htmlTable += `<td style="font-weight: ${
-        (keyChangeMeta.bold || changedProperties.includes(key)) ? 'bold' : 'normal'
-      };">${
-        (keyChangeMeta.deleted || !rowValue?.[key]) ? 'DELETED' : rowValue[key]
-      }</td>`
+      let keyChangeMeta = isChangedValue(
+        flattenPreArray,
+        flattenCurrArray,
+        key,
+        rowValue._id
+      )
+      let keyChange =
+        keyChangeMeta.changed || keyChangeMeta.added || keyChangeMeta.deleted
+      if (keyChange) {
+        htmlTable += `<td style="background-color: ${getColor(
+          keyChangeMeta
+        )}; font-weight: bold;">${
+          keyChangeMeta.deleted || !rowValue?.[key] ? 'DELETED' : rowValue[key]
+        }</td>`
+      } else {
+        htmlTable += `<td style="background-color: ${getColor(
+          keyChangeMeta
+        )};">${
+          keyChangeMeta.deleted || !rowValue?.[key] ? 'DELETED' : rowValue[key]
+        }</td>`
+      }
     })
+
     htmlTable += `</tr>`
   })
   htmlTable += `</table>`
   return htmlTable
+}
+
+const getColor = meta => {
+  if (meta.changed && meta.deleted) {
+    // This is a non-existing value in the target and that has changed in the source
+    return '#E1BEE7'
+  } else {
+    if (meta.changed) {
+      return '#B2DFDB'
+    } else if (meta.added) {
+      return '#C8E6C9'
+    } else if (meta.deleted) {
+      return '#C2185B'
+    }
+  }
+}
+
+// Generate a table for the meaning of the colors
+const generateColorTable = () => {
+  let colorTable = `<table style="width:100%"><tr>`
+  colorTable += `<th>Color</th>`
+  colorTable += `<th>Meaning</th>`
+  colorTable += `</tr>`
+  colorTable += `<tr>`
+  colorTable += `<td style="background-color: #B2DFDB;">#B2DFDB</td>`
+  colorTable += `<td>Value has changed</td>`
+  colorTable += `</tr>`
+  colorTable += `<tr>`
+  colorTable += `<td style="background-color: #C8E6C9;">#C8E6C9</td>`
+  colorTable += `<td>Value has been added</td>`
+  colorTable += `</tr>`
+  colorTable += `<tr>`
+  colorTable += `<td style="background-color: #C2185B;">#C2185B</td>`
+  colorTable += `<td>Value has been deleted</td>`
+  colorTable += `</tr>`
+  colorTable += `<tr>`
+  colorTable += `<td style="background-color: #E1BEE7;">#E1BEE7</td>`
+  colorTable += `<td>Non-existing value in the target and that has changed in the source</td>`
+  colorTable += `</tr>`
+  colorTable += `</table>`
+  return colorTable
 }
 
 // Uncomment to use to test implementation a timer to see how long it takes to run
