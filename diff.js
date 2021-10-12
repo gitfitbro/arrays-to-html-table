732733f3e89411a44d4eea7f3eb4f6533b9682a5
Sirrele Steinfeld <mssteinfeld@gmail.com>
2021-10-12 01:53:24 -0700

    Readme update
    Moved task overview and todos to the readme with better formatting

--- diff.js
+++ diff.js
@@ -3,28 +3,6 @@
  * @param {Array} prevArray
  * @param {Array} currArray
  * @returns {String} htmlTable
- * [] TODO: Move most of these comments to a README.md file
- * [x] TODO: Create a Set of _ids for currArray and PrevArray
- * [x] TODO: Use the Set() and Map() Classes
- * The function will take two arguments
- * [x] (prevArray, currArray)
- * [x] flattens the objects inside of prevArray and currArray to 1 level of depth,
- * [x] Returns an HTML Table of the values.
- *
- * HTML Table
- * [x] The HTML table you return has a column header which is a superset of all keys in all the objects in the currArray.
- * [x] Any values that have changed from the prevArray to the currArray (ie field value changed or is a new key altogether) should be bolded
- * [x] In the case that the value has been removed altogether from the prevArray to the currArray, you will write out the key in bold DELETED.
- *
- * RULES:
- * The arrays are arbitrarily deep
- * The currArray could have more or potentially even be in a different index order
- * You cannot depend solely on array index for comparison
- * you can assume that each object in the arrays will have an "_id" parameter
- *   -> Unless the currArray has no object with the matching "_id" parameter (for example if the whole row has changed)
- *  Do not create global scope
- *  We have a test runner that will iterate on your function and run many fixtures through it
- * --> If you create global scope for 1 individual diff between prevArray to currArray you could cause other tests to fail
  **/
 
 module.exports.arrayDiffToHtmlTable = function (prevArray, currArray) {
